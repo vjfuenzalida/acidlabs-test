b80cd1623605136fcff179101e993268722f4291
Vicente Fuenzalida Marín <vjfuenzalida@uc.cl>
2019-01-03 03:09:44 -0300

    correct lint highlighted errors and add rubocop config file

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
+# Main controller. It handles the main screen
+# of the application (root).
 class WelcomeController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
-  def page_title(separator = " – ")
+  def page_title(separator = ' – ')
     [content_for(:title), 'Weather App'].compact.join(separator)
   end
 end
